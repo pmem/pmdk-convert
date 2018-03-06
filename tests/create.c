a81b1e06a33e5acca4f0ed1bc17b2ba575bdaaee
Marcin Ślusarz <marcin.slusarz@intel.com>
2018-03-06 12:05:22 +0100

    tests: let user create pools without poolset file

--- tests/create.c
+++ tests/create.c
@@ -39,11 +39,15 @@ int
 main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		fprintf(stderr, "Usage: %s pool_file\n", argv[0]);
+		fprintf(stderr, "Usage: %s pool_file [size in MB]\n", argv[0]);
 		exit(1);
 	}
 
-	PMEMobjpool *pop = pmemobj_create(argv[1], "test", 0, 0755);
+	size_t sz = 0;
+	if (argc >= 3)
+		sz = (size_t)atoi(argv[2]) * 1024 * 1024;
+
+	PMEMobjpool *pop = pmemobj_create(argv[1], "test", sz, 0755);
 	if (!pop) {
 		fprintf(stderr, "pmemobj_create failed: %s\n",
 				pmemobj_errormsg());
