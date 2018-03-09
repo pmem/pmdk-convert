564da0913acac234bf44c8e86745d2ccb2283649
Marcin Ślusarz <marcin.slusarz@intel.com>
2018-03-09 13:13:09 +0100

    Initial version of convert from 1.4 to 1.5.
    1.5 doesn't exist yet, but we know we'll bump major version
    of the layout.

---
+++ pmemobj_convert_1.4_to_1.5.c
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2014-2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <endian.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
+#include "libpmem.h"
+
+#include "pmemobj_convert.h"
+#include "nvml-1.4/src/include/libpmemobj.h"
+#include "nvml-1.4/src/common/set.h"
+#include "nvml-1.4/src/tools/pmempool/common.h"
+#include "nvml-1.4/src/tools/pmempool/output.h"
+
+/*
+ * outv_err_vargs -- print error message
+ */
+void
+outv_err_vargs(const char *fmt, va_list ap)
+{
+	fprintf(stderr, "error: ");
+	vfprintf(stderr, fmt, ap);
+	if (!strchr(fmt, '\n'))
+		fprintf(stderr, "\n");
+}
+
+/*
+ * outv_err -- print error message
+ */
+void
+outv_err(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	outv_err_vargs(fmt, ap);
+	va_end(ap);
+}
+
+/*
+ * pool_set_file_unmap_headers -- unmap headers of each pool set part file
+ */
+static void
+pool_set_file_unmap_headers(struct pool_set_file *file)
+{
+	if (!file->poolset)
+		return;
+	for (unsigned r = 0; r < file->poolset->nreplicas; r++) {
+		struct pool_replica *rep = file->poolset->replica[r];
+		for (unsigned p = 0; p < rep->nparts; p++) {
+			struct pool_set_part *part = &rep->part[p];
+			util_unmap_hdr(part);
+		}
+	}
+}
+
+/*
+ * pool_set_file_map_headers -- map headers of each pool set part file
+ */
+static int
+pool_set_file_map_headers(struct pool_set_file *file, int rdonly)
+{
+	if (!file->poolset)
+		return -1;
+
+	int flags = rdonly ? MAP_PRIVATE : MAP_SHARED;
+	for (unsigned r = 0; r < file->poolset->nreplicas; r++) {
+		struct pool_replica *rep = file->poolset->replica[r];
+		for (unsigned p = 0; p < rep->nparts; p++) {
+			struct pool_set_part *part = &rep->part[p];
+			if (util_map_hdr(part, flags, 0)) {
+				part->hdr = NULL;
+				goto err;
+			}
+		}
+	}
+
+	return 0;
+err:
+	pool_set_file_unmap_headers(file);
+	return -1;
+}
+
+static void
+pmemobj_convert_persist(const void *addr, size_t size)
+{
+	/* device dax */
+	pmem_persist(addr, size);
+	/*
+	 * fs dax / nonpmem, will fail for ddax, but it doesn't
+	 * matter
+	 */
+	pmem_msync(addr, size);
+}
+
+const char *
+pmemobj_convert_14_to_15(const char *path, unsigned force)
+{
+	/* open the pool and perform recovery */
+	PMEMobjpool *pop = pmemobj_open(path, NULL);
+	if (!pop)
+		return pmemobj_errormsg();
+
+	/* now recovery state is clean, so we can zero it out */
+	struct lane_layout *lanes =
+			(struct lane_layout *)((char *)pop + pop->lanes_offset);
+	memset(lanes, 0, pop->nlanes * sizeof(struct lane_layout));
+	pmemobj_persist(pop, lanes, pop->nlanes * sizeof(struct lane_layout));
+
+	pmemobj_close(pop);
+
+	static char errstr[500];
+	const char *ret = NULL;
+	struct pmem_pool_params params;
+	if (pmem_pool_parse_params(path, &params, 1)) {
+		sprintf(errstr, "cannot open pool: %s", strerror(errno));
+		ret = errstr;
+		return ret;
+	}
+
+	struct pool_set_file *psf = pool_set_file_open(path, 0, 1);
+	if (psf == NULL) {
+		sprintf(errstr, "pool_set_file_open failed: %s",
+				strerror(errno));
+		ret = errstr;
+		return ret;
+	}
+
+	if (psf->poolset->remote) {
+		sprintf(errstr, "Conversion of remotely replicated pools is "
+			"currently not supported. Remove the replica first\n");
+		ret = errstr;
+		return ret;
+	}
+
+	void *addr = pool_set_file_map(psf, 0);
+	if (addr == NULL) {
+		ret = "mapping file failed";
+		goto pool_set_close;
+	}
+
+	struct pool_hdr *phdr = addr;
+	uint32_t m = le32toh(phdr->major);
+	if (m != OBJ_FORMAT_MAJOR) {
+		/* shouldn't be possible, because pool open succeeded earlier */
+		sprintf(errstr, "invalid pool version: %d", m);
+		ret = errstr;
+		goto pool_set_close;
+	}
+
+	if (pool_set_file_map_headers(psf, 0)) {
+		sprintf(errstr, "mapping headers failed: %s", strerror(errno));
+		ret = errstr;
+		goto pool_set_close;
+	}
+
+	/* need to update every header of every part */
+	for (unsigned r = 0; r < psf->poolset->nreplicas; ++r) {
+		struct pool_replica *rep = psf->poolset->replica[r];
+		for (unsigned p = 0; p < rep->nparts; ++p) {
+			struct pool_set_part *part = &rep->part[p];
+
+			struct pool_hdr *hdr = part->hdr;
+			assert(hdr->major == OBJ_FORMAT_MAJOR);
+			hdr->major = htole32(OBJ_FORMAT_MAJOR + 1);
+			util_checksum(hdr, sizeof(*hdr), &hdr->checksum, 1,
+					POOL_HDR_CSUM_END_OFF);
+			pmemobj_convert_persist(hdr, sizeof(struct pool_hdr));
+
+			if (le32toh(hdr->incompat_features) &
+					POOL_FEAT_SINGLEHDR)
+				break;
+		}
+	}
+
+	pool_set_file_unmap_headers(psf);
+
+pool_set_close:
+	pool_set_file_close(psf);
+
+	return ret;
+}
--- tests/basic.cmake
+++ tests/basic.cmake
@@ -35,18 +35,25 @@ function(test)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool10)
 
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool11)
 
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool12)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool12)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool12)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool12)
 
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool13)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool13)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool13)
+
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool14)
 
 	# 1.0 -> 1.1
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.0 --to=1.1 ${DIR}/pool10 -X fail-safety)
@@ -54,6 +61,7 @@ function(test)
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool10)
 
 	# 1.1 -> 1.2
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.1 --to=1.2 ${DIR}/pool10 -X fail-safety -X 1.2-pmemmutex)
@@ -61,12 +69,14 @@ function(test)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool10)
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool10)
 
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.1 --to=1.2 ${DIR}/pool11 -X fail-safety -X 1.2-pmemmutex)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool11)
 
 	# 1.2 -> 1.3
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.2 --to=1.3 ${DIR}/pool10 -X fail-safety)
@@ -92,21 +102,66 @@ function(test)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool10)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool10)
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool10)
 
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.3 --to=1.4 ${DIR}/pool11 -X fail-safety)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool11)
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool11)
 
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.3 --to=1.4 ${DIR}/pool12 -X fail-safety)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool12)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool12)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool12)
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool12)
 
 	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.3 --to=1.4 ${DIR}/pool13 -X fail-safety)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool13)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool13)
 	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool13)
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool13)
+
+	# 1.4 -> 1.5
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool10 -X fail-safety)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool10)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool10)
+	#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool10)
+
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool11 -X fail-safety)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool11)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool11)
+	#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool11)
+
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool12 -X fail-safety)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool12)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool12)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool12)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool12)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool12)
+	#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool12)
+
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool13 -X fail-safety)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool13)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool13)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool13)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool13)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool13)
+	#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool13)
+
+	execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool14 -X fail-safety)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool14)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
+	#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool14)
 endfunction(test)
 
 # argument parsing
@@ -128,16 +183,19 @@ file(WRITE ${DIR}/pool10 "PMEMPOOLSET\n16M ${DIR}/part10\n")
 file(WRITE ${DIR}/pool11 "PMEMPOOLSET\n16M ${DIR}/part11\n")
 file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12\n")
 file(WRITE ${DIR}/pool13 "PMEMPOOLSET\n16M ${DIR}/part13\n")
+file(WRITE ${DIR}/pool14 "PMEMPOOLSET\n16M ${DIR}/part14\n")
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_14 ${DIR}/pool14)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${DIR}/part10 ${DIR}/pool10)
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${DIR}/part11 ${DIR}/pool11)
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${DIR}/part12 ${DIR}/pool12)
 execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${DIR}/part13 ${DIR}/pool13)
+execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${DIR}/part14 ${DIR}/pool14)
 
 test()
 
@@ -148,16 +206,19 @@ file(WRITE ${DIR}/pool10 "PMEMPOOLSET\n16M ${DIR}/part10\n")
 file(WRITE ${DIR}/pool11 "PMEMPOOLSET\n16M ${DIR}/part11\n")
 file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12\n")
 file(WRITE ${DIR}/pool13 "PMEMPOOLSET\n16M ${DIR}/part13\n")
+file(WRITE ${DIR}/pool14 "PMEMPOOLSET\n16M ${DIR}/part14\n")
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_14 ${DIR}/pool14)
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
 
 test()
 
@@ -168,16 +229,19 @@ file(WRITE ${DIR}/pool10 "PMEMPOOLSET\n16M ${DIR}/part10_1 ${DIR}/part10_2\n")
 file(WRITE ${DIR}/pool11 "PMEMPOOLSET\n16M ${DIR}/part11_1 ${DIR}/part11_2\n")
 file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12_1 ${DIR}/part12_2\n")
 file(WRITE ${DIR}/pool13 "PMEMPOOLSET\n16M ${DIR}/part13_1 ${DIR}/part13_2\n")
+file(WRITE ${DIR}/pool14 "PMEMPOOLSET\n16M ${DIR}/part14_1 ${DIR}/part14_2\n")
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_14 ${DIR}/pool14)
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
 
 test()
 
@@ -188,17 +252,40 @@ file(WRITE ${DIR}/pool10 "PMEMPOOLSET\n16M ${DIR}/part10_rep1\nREPLICA\n16M ${DI
 file(WRITE ${DIR}/pool11 "PMEMPOOLSET\n16M ${DIR}/part11_rep1\nREPLICA\n16M ${DIR}/part11_rep2\n")
 file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12_rep1\nREPLICA\n16M ${DIR}/part12_rep2\n")
 file(WRITE ${DIR}/pool13 "PMEMPOOLSET\n16M ${DIR}/part13_rep1\nREPLICA\n16M ${DIR}/part13_rep2\n")
+file(WRITE ${DIR}/pool14 "PMEMPOOLSET\n16M ${DIR}/part14_rep1\nREPLICA\n16M ${DIR}/part14_rep2\n")
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_14 ${DIR}/pool14)
 
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_10 ${DIR}/pool10)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_11 ${DIR}/pool11)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_12 ${DIR}/pool12)
 execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_13 ${DIR}/pool13)
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
 
 test()
+
+# multi file poolset with local replica and SINGLEHDR option
+setup()
+
+file(WRITE ${DIR}/pool14
+	"PMEMPOOLSET\n"
+	"OPTION SINGLEHDR\n"
+	"16M ${DIR}/part14_part1_rep1\n"
+	"16M ${DIR}/part14_part2_rep1\n"
+	"REPLICA\n"
+	"16M ${DIR}/part14_part1_rep2\n"
+	"16M ${DIR}/part14_part2_rep2\n")
+
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/create_14 ${DIR}/pool14)
+
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
+
+execute(1 ${CMAKE_CURRENT_BINARY_DIR}/../pmdk-convert --from=1.4 --to=1.5 ${DIR}/pool14 -X fail-safety)
+execute(0 ${CMAKE_CURRENT_BINARY_DIR}/open_14 ${DIR}/pool14)
+#execute(1 ${CMAKE_CURRENT_BINARY_DIR}/open_15 ${DIR}/pool14)
 
 cleanup()
