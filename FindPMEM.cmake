3f056b1595401848d5eaf870d9bdd98458f4c13a
Łukasz Plewa <lukasz.plewa@intel.com>
2018-10-01 14:02:46 +0200

    port convert to windows

--- FindPMEM.cmake
+++ FindPMEM.cmake
@@ -29,61 +29,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include(${SRC_DIR}/helpers.cmake)
+find_path(PMEM_INCLUDE_DIRS libpmem.h)
+find_library(PMEM_LIBRARIES NAMES pmem libpmem)
 
-function(test)
-	check_open(${DIR}/pool12 "1.2")
-
-	# 1.2 -> 1.3
-	execute(0 ${EXE_DIR}/pmdk-convert --to=1.3 ${DIR}/pool12 -X fail-safety)
-	check_open(${DIR}/pool12 "1.3 1.4")
-
-	# 1.3 -> 1.4
-	execute(0 ${EXE_DIR}/pmdk-convert --to=1.4 ${DIR}/pool12 -X fail-safety)
-	check_open(${DIR}/pool12 "1.3 1.4")
-
-	# 1.4 -> 1.5
-	execute(0 ${EXE_DIR}/pmdk-convert --to=1.5 ${DIR}/pool12 -X fail-safety)
-	check_open(${DIR}/pool12 "1.5")
-endfunction(test)
-
-# single file pool
-setup()
-
-execute(0 ${TEST_DIR}/create_12 ${DIR}/pool12 16)
-
-test()
-
-# single file poolset
-setup()
-
-file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12\n")
-
-execute(0 ${TEST_DIR}/create_12 ${DIR}/pool12)
-
-test()
-
-# multi file poolset
-setup()
-
-file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12_1\n16M ${DIR}/part12_2\n")
-
-execute(0 ${TEST_DIR}/create_12 ${DIR}/pool12)
-
-test()
-
-# poolset with local replica
-setup()
-
-file(WRITE ${DIR}/pool12 "PMEMPOOLSET\n16M ${DIR}/part12_rep1\nREPLICA\n16M ${DIR}/part12_rep2\n")
-execute(0 ${TEST_DIR}/create_12 ${DIR}/pool12)
-
-test()
-
-# invalid pool
-setup()
-
-file(WRITE ${DIR}/not_a_pool "This is not a pool\n")
-execute(15 ${EXE_DIR}/pmdk-convert ${DIR}/not_a_pool -X fail-safety --from 1.2) # CONVERT_FAILED
-
-cleanup()
+mark_as_advanced(PMEM_LIBRARIES PMEM_INCLUDE_DIRS)
