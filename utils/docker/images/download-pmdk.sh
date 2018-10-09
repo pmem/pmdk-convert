8eb19a02f3215bc2783a4d7b9d7a3da32e426c44
Łukasz Plewa <lukasz.plewa@intel.com>
2018-10-09 15:05:52 +0200

    Switch back to NVML 1.4.2

--- utils/docker/images/download-pmdk.sh
+++ utils/docker/images/download-pmdk.sh
@@ -38,7 +38,7 @@ set -e
 
 mkdir /opt/pmdk
 
-wget https://github.com/pmem/pmdk/archive/b7c4d0ae08f7f535e6f8a2b789c2ad3db0ee0858.tar.gz -O /opt/pmdk/nvml-1.4.2.tar.gz
+wget https://github.com/pmem/pmdk/archive/1.4.2.tar.gz -O /opt/pmdk/nvml-1.4.2.tar.gz
 wget https://github.com/pmem/pmdk/archive/1.3.1.tar.gz -O /opt/pmdk/nvml-1.3.1.tar.gz
 wget https://github.com/pmem/pmdk/archive/1.2.3.tar.gz -O /opt/pmdk/nvml-1.2.3.tar.gz
 wget https://github.com/pmem/pmdk/archive/1.1.tar.gz -O /opt/pmdk/nvml-1.1.tar.gz
