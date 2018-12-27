1dafbb90ebf1c3c06ca1ee0a1e01ae95242bb38d
Maciej Pietras <maciejx.pietras@intel.com>
2018-12-27 15:32:52 +0100

    tests: add pmempool convert with pmemspoil test

---
+++ tests/spoiled_pool_hdr.cmake
@@ -0,0 +1,84 @@
+#
+# Copyright 2018 - 2019, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+include(${SRC_DIR}/helpers.cmake)
+
+function(read_expected_major output)
+	list(LENGTH VERSIONS len)
+	math(EXPR len ${len}-1)
+	list(GET VERSIONS ${len} newest_version)
+	string(REGEX REPLACE "[0-9]+." "" out ${newest_version})
+	set(${output} ${out} PARENT_SCOPE)
+endfunction()
+
+setup()
+
+list(LENGTH VERSIONS num)
+math(EXPR last_position "${num} - 1")
+list(GET VERSIONS ${last_position}-1 newest_version)
+set(index 1)
+if(WIN32)
+	list(GET VERSIONS 1.4 min_win_version)
+	string(REPLACE "." "" min_version ${min_win_version})
+else()
+	list(GET VERSIONS 1.0 min_linux_version)
+	string(REPLACE "." "" min_version ${min_linux_version})
+endif()
+
+while(index LESS num)
+	list(GET VERSIONS ${index} curr_version)
+	string(REPLACE "." "" bin_version ${curr_version})
+
+	if(NOT(bin_version LESS min_version))
+		execute(0 ${TEST_DIR}/create_${bin_version} ${DIR}/pool${bin_version} 16)
+
+		execute(0 ${TEST_DIR}/pmemspoil ${DIR}/pool${bin_version} pool_hdr.major=0)
+		execute(11 ${EXE_DIR}/pmdk-convert ${DIR}/pool${bin_version} -X fail-safety -X 1.2-pmemmutex)
+		execute(1 ${TEST_DIR}/pmempool-convert info ${DIR}/pool${bin_version})
+
+		read_expected_major(expected_major)
+		math(EXPR incorrect_version "${expected_major} + 1")
+
+		execute(0 ${TEST_DIR}/pmemspoil ${DIR}/pool${bin_version} pool_hdr.major=${incorrect_version})
+		execute(11 ${EXE_DIR}/pmdk-convert ${DIR}/pool${bin_version} -X fail-safety -X 1.2-pmemmutex)
+		execute(1 ${TEST_DIR}/pmempool-convert info ${DIR}/pool${bin_version})
+
+		if(${curr_version} EQUAL ${newest_version})
+			execute(0 ${EXE_DIR}/pmdk-convert ${DIR}/pool${bin_version} -X fail-safety -X 1.2-pmemmutex --from ${curr_version})
+		else()
+			execute(15 ${EXE_DIR}/pmdk-convert ${DIR}/pool${bin_version} -X fail-safety -X 1.2-pmemmutex --from ${curr_version})
+		endif()
+
+		MATH(EXPR index "${index} + 1")
+	endif()
+endwhile()
+
+cleanup()
