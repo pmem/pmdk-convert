d441948677c0720360163378b8536f62a1b944b1
mjareckx <michalx.jarecki@intel.com>
2019-01-29 12:18:36 +0100

    Create debug tests for windows and execute them author mjareckx <michalx.jarecki@intel.com> 1548760716 +0100 committer karolxkrzyzanowski <karol.krzyzanowski@intel.com> 1551278361 +0100
    Editing tests for Windows
    
    Cdb debbuger works with executions
    
    Added DTEST flag
    
    Decrease macro BIG_ALLOC, cdb on release not debugging

--- tests/transactionW/transactionW.c
+++ tests/transactionW/transactionW.c
@@ -42,9 +42,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#ifdef _WIN32
 #include <string>
-#endif
 #include "libpmemobj.h"
 
 POBJ_LAYOUT_BEGIN(convert);
@@ -590,26 +588,18 @@ main(int argc, char *argv[])
 		printf("usage: %s file [c|va|vc] scenario", argv[0]);
 
 	const char *path = argv[1];
-#ifdef _WIN32
+
 	std::wstring wc(strlen(path), L'#');
 	mbstowcs(&wc[0], path, strlen(path)+1);
-#endif 
 	int prepare_pool = argv[2][0] == 'c';
 	int verify_abort = argv[2][1] == 'a';
 	int sc = atoi(argv[3]);
 
 	PMEMobjpool *pop = NULL;
-#ifdef _WIN32
 	if ((pop = pmemobj_open(wc.c_str(), NULL)) == NULL) {
-		printf("failed to open pool\n");
-		exit(24);
-	}
-#else
-	if ((pop = pmemobj_open(path, NULL)) == NULL) {
 			printf("failed to open pool\n");
 			exit(24);
 	}
-#endif
 
 	if (prepare_pool)
 		scenarios[sc].create(pop);
