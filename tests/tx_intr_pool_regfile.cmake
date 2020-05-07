0ff8520c127be11b64329ad6b66df12744b34245
osalyk <oksana.salyk@intel.com>
2020-05-07 16:34:57 +0200

    test: update tests which use the test_tx_intr function

--- tests/tx_intr_pool_regfile.cmake
+++ tests/tx_intr_pool_regfile.cmake
@@ -1,22 +1,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2018-2019, Intel Corporation
+# Copyright 2018-2020, Intel Corporation
 
 include(${SRC_DIR}/helpers.cmake)
 
 # prepare single file pools for testing for each version of PMDK
-function(prepare_files)
+function(prepare_files bin_version)
 	setup()
 
-	foreach(version ${VERSIONS})
-		string(REPLACE "." "" bin_version ${version})
+	execute(0 ${TEST_DIR}/create_${bin_version}
+		${DIR}/pool${bin_version}a 16)
 
-		execute(0 ${TEST_DIR}/create_${bin_version}
-			${DIR}/pool${bin_version}a 16)
-
-		execute(0 ${TEST_DIR}/create_${bin_version}
-			${DIR}/pool${bin_version}c 32)
-	endforeach()
-
+	execute(0 ${TEST_DIR}/create_${bin_version}
+		${DIR}/pool${bin_version}c 32)
 endfunction()
 
 test_intr_tx(prepare_files)
--- tests/tx_intr_poolset_local_2regfiles_rep_3regfiles.cmake
+++ tests/tx_intr_poolset_local_2regfiles_rep_3regfiles.cmake
@@ -1,18 +1,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2018-2019, Intel Corporation
+# Copyright 2018-2020, Intel Corporation
 
 include(${SRC_DIR}/helpers.cmake)
 
 # prepare poolsets with local replica on regular files for testing for
 # each version of PMDK
-function(prepare_files)
+function(prepare_files bin_version)
 	setup()
 
-	foreach(version ${VERSIONS})
-		string(REPLACE "." "" bin_version ${version})
-
-		file(WRITE ${DIR}/pool${bin_version}a
-			"PMEMPOOLSET
+	file(WRITE ${DIR}/pool${bin_version}a
+		"PMEMPOOLSET
 32M ${DIR}/part${bin_version}a_1
 16M ${DIR}/part${bin_version}a_2
 REPLICA
@@ -20,11 +17,11 @@ REPLICA
 8M ${DIR}/part${bin_version}a_4
 40M ${DIR}/part${bin_version}a_5")
 
-		execute(0 ${CMAKE_CURRENT_BINARY_DIR}/create_${bin_version}
-				${DIR}/pool${bin_version}a)
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/create_${bin_version}
+		${DIR}/pool${bin_version}a)
 
-		file(WRITE ${DIR}/pool${bin_version}c
-			"PMEMPOOLSET
+	file(WRITE ${DIR}/pool${bin_version}c
+		"PMEMPOOLSET
 128M ${DIR}/part${bin_version}c_1
 48M ${DIR}/part${bin_version}c_2
 REPLICA
@@ -32,10 +29,8 @@ REPLICA
 16M ${DIR}/part${bin_version}c_4
 32M ${DIR}/part${bin_version}c_5")
 
-		execute(0 ${CMAKE_CURRENT_BINARY_DIR}/create_${bin_version}
-				${DIR}/pool${bin_version}c)
-	endforeach()
-
+	execute(0 ${CMAKE_CURRENT_BINARY_DIR}/create_${bin_version}
+			${DIR}/pool${bin_version}c)
 endfunction()
 
 test_intr_tx(prepare_files)
